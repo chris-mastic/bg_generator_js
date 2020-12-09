5acb1bc787d5fad4918754371f68be87ae04a46c
Chris Mazza <chrismazza@pop-os.localdomain>
2020-12-09 04:34:29 -0600

    added package.json and lodash dependecy as well as simple build scirpt

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+//using the 'lodash' package
+var _ = require('lodash');
+
+var ar = [1,2,3,4,5,6,7,8];
+console.log('answer:', _.without(ar,3));
+//finish using the lodash packgae.  In order to actually use it you must
+//execute 'browserify script.js > bundle.js' in shell and also be sure
+//to change any script.js reference in html to bundle.js since all the
+//js files are now bundled in the 'bundle.js' file.  remember, browserify
+//is a more advance (but older) method to use javascript in files.  Used 
+//to be inline tags or multiple js files referenced via the <link> tag.
+//then is wa iife, next browseify and finally 'webapck'
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
